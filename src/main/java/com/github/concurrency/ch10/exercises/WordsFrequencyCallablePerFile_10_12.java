ecce6448f50af93a6f30aa7227ab99a89e87af78
Oleksandr Zakharchenko <Oleksandr.Zakharchenko@playtech.com>
2017-09-06 18:07:59 +0300

    10.12 updated

--- src/main/java/com/github/concurrency/ch10/exercises/WordsFrequencyCallablePerFile_10_12.java
+++ src/main/java/com/github/concurrency/ch10/exercises/WordsFrequencyCallablePerFile_10_12.java
@@ -1,19 +1,19 @@
 package com.github.concurrency.ch10.exercises;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.*;
 import java.util.stream.Stream;
 
 public class WordsFrequencyCallablePerFile_10_12 {
 
     public static final int NUM_OF_READERS = 4;
-    public static final File DUMMY = new File("<DUMMY>");
+    //    public static final File DUMMY = new File("<DUMMY>");
     private BlockingQueue<File> filesMap = new LinkedBlockingQueue<>();
     private BlockingQueue<Map<String, Integer>> queueOfMaps = new LinkedBlockingQueue<>();
 
@@ -28,22 +28,44 @@ public class WordsFrequencyCallablePerFile_10_12 {
         new WordsFrequencyCallablePerFile_10_12().execute(path);
     }
 
-    private void execute(String path) throws Exception {
-        Stream<Path> files = Files.list(Paths.get(path));
-        executeWriter(files);
+    private void execute(String basePath) throws Exception {
+        Stream<Path> paths = Files.list(Paths.get(basePath));
+        executeWriter(paths);
 
-        int numOfFiles = Long.valueOf(files.count()).intValue();
+        int numOfFiles = Long.valueOf(paths.count()).intValue();
         pool = Executors.newFixedThreadPool(numOfFiles);
 
         Collection<Callable<Map<String, Integer>>> callables = new ArrayList<>(numOfFiles);
-        files.forEach(file -> {
+        paths.forEach(path -> {
             Callable<Map<String, Integer>> callable = () -> {
-                return null;//TODO implement
+
+                Map<String, Integer> map = new HashMap<>();
+                countFrequency(map, path.toFile());
+                return map;
             };
             callables.add(callable);
         });
 
-        pool.invokeAll(callables);
+        Map<String, Integer> aggregation = new HashMap<>();
+        List<Future<Map<String, Integer>>> futures = pool.invokeAll(callables);
+        for (Future<Map<String, Integer>> future : futures) {
+            Map<String, Integer> map = future.get();
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                Integer result = map.get(entry.getKey());
+                Integer finalResult;
+                if (result != null) {
+                    finalResult = entry.getValue() + result;
+                } else {
+                    finalResult = entry.getValue();
+                }
+                aggregation.put(entry.getKey(), finalResult);
+            }
+        }
+        System.out.println(">>> result: ");
+        Iterator<Map.Entry<String, Integer>> iterator = aggregation.entrySet().iterator();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(">>> " + iterator.next());
+        }
     }
 
     private void executeWriter(Stream<Path> files) {
@@ -64,14 +86,25 @@ public class WordsFrequencyCallablePerFile_10_12 {
                 throw new RuntimeException(e);
             }
 
-            //When the producer is done, it should put a dummy file into the queue.
-            try {
-                filesMap.put(DUMMY);
-                System.out.println(">>> put DUMMY to queue <<<");
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+//            //When the producer is done, it should put a dummy file into the queue.
+//            try {
+//                filesMap.put(DUMMY);
+//                System.out.println(">>> put DUMMY to queue <<<");
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
         }, pool);
     }
 
+    private void countFrequency(Map<String, Integer> map, File file) throws IOException {
+        Files.readAllLines(Paths.get(file.getAbsolutePath()), Charset.forName("utf-8"))
+                .stream()
+                .flatMap((list) -> Stream.of(list.split(" ")))
+                .forEach(w -> {
+
+                    Integer frequency = Optional.ofNullable(map.get(w)).orElse(0) + 1;
+                    map.put(w, frequency);
+
+                });
+    }
 }
